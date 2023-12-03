1d672cf60774ae28d3801046de526bb5b41a1e32
Suzana <suzanasouza.caetano@gmail.com>
2023-12-03 00:36:23 -0300

    using fs.writeFile()

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const fs = require("fs") // file system
 
+// Lendo arquivo
 fs.readFile("./suzana.araujo",{encoding: 'utf-8'},(erro, dados)=>{
     if (erro) {
         console.log("Houve uma falha na leitura do arquivo")
     }
     else {
         console.log(dados)
+    }
+})
+
+// Escrevendo arquivo
+let conteudo = "Mas tenho medo de dirigir moto"
+fs.writeFile("./suzana.araujo",conteudo,(erro)=>{
+    if(erro){
+        console.log("Houve um erro no registro do arquivo.")
+    }
+    else {
+        console.log("Registro feito com sucesso!")
     }
 })
