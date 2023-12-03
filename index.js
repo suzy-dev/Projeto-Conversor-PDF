4ad4a22ca1694ec05d2f1ce42f02b233bd6076a9
Suzana <suzanasouza.caetano@gmail.com>
2023-12-03 01:03:12 -0300

    alterando chave do objeto json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require("fs") // file system
 
-// Lendo arquivo
+/* Lendo arquivo
 fs.readFile("./suzana.araujo",{encoding: 'utf-8'},(erro, dados)=>{
     if (erro) {
         console.log("Houve uma falha na leitura do arquivo")
@@ -18,5 +18,21 @@ fs.writeFile("./suzana.araujo",conteudo,(erro)=>{
     }
     else {
         console.log("Registro feito com sucesso!")
+    }
+})
+
+// Leitor e Escrito de arquivo .json */
+
+fs.readFile("./file.json",'utf8',(erro, dados)=>{
+    if(erro){
+        console.log("Houve um erro na leitura do arquivo json", erro)
+    }
+    else {
+        const objetoJson = JSON.parse(dados) //convertendo txt p objeto javascript
+        console.log(typeof objetoJson)       // verificando se converteu p objeto
+
+        objetoJson.nome = "Suzana Araujo"    //alterando chave nome 
+        objetoJson.curso = ["Ciência da Computação","Ciência de Dados"] 
+        console.log(objetoJson)
     }
 })
