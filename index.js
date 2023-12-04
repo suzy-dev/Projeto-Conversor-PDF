8ee542ce6a38d185ce2ce92a03de8a032d94d347
Suzana <suzanasouza.caetano@gmail.com>
2023-12-04 09:00:39 -0300

    lendo e escrevendo arqv json
    arquivo csv

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ fs.writeFile("./suzana.araujo",conteudo,(erro)=>{
     }
 })
 
-// Leitor e Escrito de arquivo .json */
+// Leitor e Escrito de arquivo .json 
 
 fs.readFile("./file.json",'utf8',(erro, dados)=>{
     if(erro){
@@ -46,3 +46,33 @@ fs.readFile("./file.json",'utf8',(erro, dados)=>{
         console.log(objetoJson)
     }
 })
+*/
+
+function modificaUsuario(nome,idade,data_nasc,curso){
+    fs.readFile("./file.json",'utf8',(erro, dados)=>{
+        if(erro){
+            console.log("Houve um erro na leitura do arquivo json", erro)
+        }
+        else {
+            const objetoJson = JSON.parse(dados) //convertendo txt p objeto javascript
+            console.log(typeof objetoJson)       // verificando se converteu p objeto
+    
+            objetoJson.nome = nome    //alterando chave nome 
+            objetoJson.curso = curso
+            objetoJson.idade = idade
+            objetoJson.data_nascimento = data_nasc
+    
+            fs.writeFile("./file.json", JSON.stringify(objetoJson),(erro)=>{    //strinfify > objeto p txt
+                if(erro){
+                    console.log("Não foi possível escrever o arquivo")
+                }
+                else{
+                    console.log("Arquivo escrito com sucesso!")
+                }
+            })
+    
+            console.log(objetoJson)
+        }
+    })
+}
+
