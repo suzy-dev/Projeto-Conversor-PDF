eeb3a076ca372117f1fcbcb07f67d9b1f3c4999d
suzana souza <suzanasouza.caetano@gmail.com>
2024-02-10 15:19:46 -0300

    renderizando tbody da table

--- HTMLParser.js
+++ HTMLParser.js
@@ -2,7 +2,7 @@ const ejs = require('ejs')
 
 class HTMLParser {
     static async Parse(table){
-        return await ejs.renderFile("./table.ejs",{header: table.header, row: table.row})
+        return await ejs.renderFile("./table.ejs",{header: table.header, row: table.rows})
     }
 }
 
